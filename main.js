de13b6802d629229d1942ebd8bc8df9af13c587b
ntsukada <n.tsukada153@gmail.com>
2024-02-10 17:02:41 -0500

    added js to the index.html page

--- main.js
+++ main.js
@@ -35,3 +35,23 @@ for (const elem of varArray) {
         }
     })
 }
+
+/*show hide menu section*/
+
+const arrow = document.getElementById("arrow");
+const menuContent = document.getElementById("menu-items");
+console.log(menuContent)
+let menuCss = window.getComputedStyle(menuContent,null)
+let menuDisplay = menuCss.getPropertyValue("display")
+arrow.addEventListener('click', ()=>{
+    if (menuDisplay === 'none') {
+        menuContent.style.setProperty("display",'flex');
+        menuDisplay = 'flex';
+        arrow.innerHTML = '<';
+    } else {
+        menuContent.style.setProperty("display",'none');
+        menuDisplay = 'none';
+        arrow.innerHTML = '>'
+    }
+})
+
